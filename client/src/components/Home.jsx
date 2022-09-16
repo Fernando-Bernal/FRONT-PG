609c196aac477ac5010ec154ed1e268f56158e95
Luis Falconi <luism.falconi@gmail.com>
2022-09-15 21:54:21 -0300

    refactor(home, firebase): add analitycs

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,9 +1,17 @@
+import { useEffect } from 'react';
+import { analytics } from '../firebase';
+import { logEvent } from 'firebase/analytics';
 import Catalog from "./Catalog";
 import NavBar from "./NavBar";
 import Footer from "./Footer";
 import Carousel from "./Carousel";
 
 function Home() {
+
+    useEffect(()=>{
+        logEvent(analytics,'HOME |S.P|')
+    },[])
+    
     return ( 
     <div>
         <NavBar/>
