d20e616d6e71e0e799b3ebaa8f89d35a865cd050
Luis Falconi <luism.falconi@gmail.com>
2022-09-20 23:15:12 -0300

    refactor(components/checkout,address): refactor checkout form controller and delete address.jsx

--- client/src/components/Checkout.jsx
+++ client/src/components/Checkout.jsx
@@ -12,7 +12,6 @@ import { limpiarCarrito } from "../redux/actions/actions";
 import { UserAuth } from "../context/AuthContext";
 import axios from "axios";
 import Swal from "sweetalert2";
-import Address from "./Address";
 
 function checkout({ products, subTotal }) {
   const stripePromise = loadStripe(
@@ -27,6 +26,66 @@ function checkout({ products, subTotal }) {
     const navigate = useNavigate();
     const [isProcessing, setProcessingTo] = useState(false);
     const [isDisable, setIsDisableTo] = useState(false);
+    const [data, setData] = useState({
+      state: "",
+      validateState: null,
+      city: "",
+      validateCity: null,
+      line1: "",
+      validateLine1: null,
+      postal_code: "",
+      validatePostal_code: null,
+      name: "",
+      validateName: null,
+    });
+
+    const handleInputChange = (e) => {
+      setData({
+        ...data,
+        [e.target.name]: e.target.value,
+      });
+    };
+
+    const expresiones = {
+      nombre: /^[a-zA-ZÀ-ÿ\s]{3,40}$/, // Letras y espacios, pueden llevar acentos.
+      line1: /^[a-zA-Z0-9\_\-]{4,16}$/, // Letras, numeros, guion y guion_bajo
+    };
+
+    const validationName = () => {
+      if (expresiones.nombre.test(data.name)) {
+        setData({ ...data, validateName: "true" });
+      } else {
+        setData({ ...data, validateName: "false" });
+      }
+    };
+    const validationState = () => {
+      if (expresiones.nombre.test(data.state)) {
+        setData({ ...data, validateState: "true" });
+      } else {
+        setData({ ...data, validateState: "false" });
+      }
+    };
+    const validationCity = () => {
+      if (expresiones.nombre.test(data.city)) {
+        setData({ ...data, validateCity: "true" });
+      } else {
+        setData({ ...data, validateCity: "false" });
+      }
+    };
+    const validationLine1 = () => {
+      if (expresiones.line1.test(data.line1)) {
+        setData({ ...data, validateLine1: "true" });
+      } else {
+        setData({ ...data, validateLine1: "false" });
+      }
+    };
+    const validationPostal_code = () => {
+      if (expresiones.line1.test(data.postal_code)) {
+        setData({ ...data, validatePostal_code: "true" });
+      } else {
+        setData({ ...data, validatePostal_code: "false" });
+      }
+    };
 
     const user = {
       email: UserAuth()?.user?.email,
@@ -67,13 +126,23 @@ function checkout({ products, subTotal }) {
 
       setProcessingTo(true);
 
-      if (!error && products !== [] && subTotal !== 0 && subTotal > 0) {
+      if (
+        !error &&
+        products !== [] &&
+        subTotal !== 0 &&
+        subTotal > 0 &&
+        data.validateName === "true" &&
+        data.validateState === "true" &&
+        data.validateCity === "true" &&
+        data.validateLine1 === "true" &&
+        data.validatePostal_code === "true"
+      ) {
         if (!error && user.email !== undefined && user.uid !== undefined) {
           console.log(paymentMethod);
           const { id } = paymentMethod;
           try {
             const { data } = await axios.post(
-              "https://sneakers-back-end.herokuapp.com/cart/checkout",
+              "https://sneakers-back-end.herokuapp.com/checkout",
               {
                 id,
                 email: user.email,
@@ -82,8 +151,7 @@ function checkout({ products, subTotal }) {
                 amount: subTotal * 100,
               }
             );
-
-            console.log(data)
+            console.log(paymentMethod);
             Swal.fire({
               position: "top-center",
               title: `${data.message}`,
@@ -93,7 +161,6 @@ function checkout({ products, subTotal }) {
             setProcessingTo(false);
             setIsDisableTo(false);
             console.log(data);
-            console.log(paymentMethod);
             elements.getElement(CardElement).clear();
             dispatch(limpiarCarrito());
           } catch (error) {
@@ -158,72 +225,158 @@ function checkout({ products, subTotal }) {
     };
 
     return (
-      <form onSubmit={handleSubmit}>
-        <Address />
-
-        <span className="text-xl font-medium text-gray-100 block py-3">
-          Card Details
-        </span>
-
-        <span className="text-xs text-gray-400 ">Card Type</span>
-
-        <div className="overflow-visible flex justify-between items-center mt-2">
-          <div className="rounded w-52 h-28 bg-gray-500 py-2 px-4 relative right-10">
-            <span className="italic text-lg font-medium text-gray-200 underline">
-              VISA
+      <div>
+        <form onSubmit={handleSubmit}>
+          <div className="pb-5 bg-[#2f3436] rounded-t border-b border-gray-400 overflow-visible">
+            <span className="text-xl font-medium text-gray-100 block pb-3">
+              Address
             </span>
 
-            <div className="flex justify-between items-center pt-4 ">
-              <span className="text-xs text-gray-200 font-medium">****</span>
-              <span className="text-xs text-gray-200 font-medium">****</span>
-              <span className="text-xs text-gray-200 font-medium">****</span>
-              <span className="text-xs text-gray-200 font-medium">****</span>
+            <div className="flex justify-center flex-col pt-3">
+              <label className="text-xs text-gray-400 ">State</label>
+              <input
+                type="text"
+                name="state"
+                onChange={handleInputChange}
+                onBlur={validationState}
+                onKeyUp={validationState}
+                className="focus:outline-none w-full h-6 bg-gray-800 text-white placeholder-gray-300 text-sm border-b border-gray-600 py-4"
+                placeholder="Arizona"
+              />
+              {data.validateState === "false" && (
+                <p className="text-xs px-1  text-red-500">
+                  The state can only contain letters and spaces
+                </p>
+              )}
             </div>
 
-            <div className="flex justify-between items-center mt-3">
-              <span className="text-xs  text-gray-200">Giga Tamarashvili</span>
-              <span className="text-xs  text-gray-200">12/18</span>
+            <div className="flex justify-center flex-col pt-3">
+              <label className="text-xs text-gray-400 ">City</label>
+              <input
+                type="text"
+                name="city"
+                onChange={handleInputChange}
+                onBlur={validationCity}
+                onKeyUp={validationCity}
+                className="focus:outline-none w-full h-6 bg-gray-800 text-white placeholder-gray-300 text-sm border-b border-gray-600 py-4"
+                placeholder="Tucson"
+              />
+              {data.validateCity === "false" && (
+                <p className="text-xs px-1  text-red-500">
+                  The city can only contain letters and spaces
+                </p>
+              )}
+            </div>
+
+            <div className="flex justify-center flex-col pt-3">
+              <label className="text-xs text-gray-400 ">Line 1</label>
+              <input
+                type="text"
+                name="line1"
+                onChange={handleInputChange}
+                onBlur={validationLine1}
+                onKeyUp={validationLine1}
+                className="focus:outline-none w-full h-6 bg-gray-800 text-white placeholder-gray-300 text-sm border-b border-gray-600 py-4"
+                placeholder="North Stone Avenue & W Alturas St"
+              />
+              {data.validateLine1 === "false" && (
+                <p className="text-xs px-1  text-red-500">
+                  enter the line 1 correctly
+                </p>
+              )}
+            </div>
+
+            <div className="flex justify-center flex-col pt-3">
+              <label className="text-xs text-gray-400 ">Postal code</label>
+              <input
+                type="text"
+                name="postal_code"
+                onChange={handleInputChange}
+                onBlur={validationPostal_code}
+                onKeyUp={validationPostal_code}
+                className="focus:outline-none w-full h-6 bg-gray-800 text-white placeholder-gray-300 text-sm border-b border-gray-600 py-4"
+                placeholder="AZ85705"
+              />
+              {data.validatePostal_code === "false" && (
+                <p className="text-xs px-1  text-red-500">
+                  enter the postal code correctly
+                </p>
+              )}
+            </div>
+          </div>
+          <span className="text-xl font-medium text-gray-100 block py-3">
+            Card Details
+          </span>
+
+          <span className="text-xs text-gray-400 ">Card Type</span>
+
+          <div className="overflow-visible flex justify-between items-center mt-2">
+            <div className="rounded w-52 h-28 bg-gray-500 py-2 px-4 relative right-10">
+              <span className="italic text-lg font-medium text-gray-200 underline">
+                VISA
+              </span>
+
+              <div className="flex justify-between items-center pt-4 ">
+                <span className="text-xs text-gray-200 font-medium">****</span>
+                <span className="text-xs text-gray-200 font-medium">****</span>
+                <span className="text-xs text-gray-200 font-medium">****</span>
+                <span className="text-xs text-gray-200 font-medium">****</span>
+              </div>
+
+              <div className="flex justify-between items-center mt-3">
+                <span className="text-xs  text-gray-200">
+                  Giga Tamarashvili
+                </span>
+                <span className="text-xs  text-gray-200">12/18</span>
+              </div>
+            </div>
+
+            <div className="flex justify-center  items-center flex-col">
+              <img
+                src="https://img.icons8.com/color/96/000000/mastercard-logo.png"
+                width="40"
+                alt=""
+                className="relative right-5"
+              />
+              <span className="text-xs font-medium text-gray-200 bottom-2 relative right-5">
+                mastercard.
+              </span>
             </div>
           </div>
 
-          <div className="flex justify-center  items-center flex-col">
-            <img
-              src="https://img.icons8.com/color/96/000000/mastercard-logo.png"
-              width="40"
-              alt=""
-              className="relative right-5"
+          <div className="flex justify-center flex-col pt-3">
+            <input
+              type="text"
+              name="name"
+              onChange={handleInputChange}
+              onBlur={validationName}
+              onKeyUp={validationName}
+              className="focus:outline-none w-full h-6 bg-gray-800 text-white placeholder-gray-300 text-sm border-b border-gray-600 py-4"
+              placeholder="Giga Tamarashvili"
             />
-            <span className="text-xs font-medium text-gray-200 bottom-2 relative right-5">
-              mastercard.
-            </span>
+            {data.validateName === "false" && (
+              <p className="text-xs px-1  text-red-500">
+                Por favor ingrese un nombre valido.
+              </p>
+            )}
           </div>
-        </div>
 
-        <div className="flex justify-center flex-col pt-3">
-          <label className="text-xs text-gray-400 ">Name on Card</label>
-          <input
-            type="text"
-            name="name"
-            className="focus:outline-none w-full h-6 bg-gray-800 text-white placeholder-gray-300 text-sm border-b border-gray-600 py-4"
-            placeholder="Giga Tamarashvili"
-          />
-        </div>
+          <div className="flex justify-center flex-col pt-3">
+            <CardElement
+              className=" p-2 border bg-gray-800 border-gray-600 hover:border-blue-600"
+              options={cardElementOption}
+            />
+          </div>
 
-        <div className="flex justify-center flex-col pt-3">
-          <CardElement
-            className=" p-2 border bg-gray-800 border-gray-600 hover:border-blue-600"
-            options={cardElementOption}
-          />
-        </div>
-
-        <button
-          type="submit"
-          className="h-12 w-full mt-3 bg-[#00ff01] rounded focus:outline-none text-white hover:bg-blue-600 disabled:opacity-50 disabled:bg-[#2f3436]"
-          disabled={isDisable}
-        >
-          {isProcessing ? "Proccesading..." : `PAY $${subTotal}`}
-        </button>
-      </form>
+          <button
+            type="submit"
+            className="h-12 w-full mt-3 bg-[#00ff01] rounded focus:outline-none text-white hover:bg-blue-600 disabled:opacity-50 disabled:bg-[#2f3436]"
+            disabled={isDisable}
+          >
+            {isProcessing ? "Proccesading..." : `PAY $${subTotal}`}
+          </button>
+        </form>
+      </div>
     );
   };
 
